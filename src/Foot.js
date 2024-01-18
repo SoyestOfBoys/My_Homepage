75b47dd9703ba0c5a4738b8a5109dad83d106e1b
SoyestOfBoys <135057721+SoyestOfBoys@users.noreply.github.com>
2024-01-18 11:46:18 -0800

    Temp removed pages to find source of AWS compiling issue

--- src/Foot.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-
-export default function Foot(){
-    return(
-        <body>
-            <div className='LeftFoot'></div>
-        </body>
-    )
-}
--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from './Pages/Home/Home'
-import GenCon2022 from './Pages/Marketing/MarketingPages/GenCon2022'
 
 const Main = () => {
 return(
     <main>
             <Routes>
                 <Route  path='/' element={<Home />} />
-                <Route path='GC22' element={<GenCon2022 />} />
             </Routes>
     </main>
 
