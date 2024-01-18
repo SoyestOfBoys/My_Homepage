2d6bc8e0f92431eeefadcf21a44ecf513add2fc3
SoyestOfBoys <135057721+SoyestOfBoys@users.noreply.github.com>
2024-01-18 15:59:25 -0800

    GC22 pt.3 Images readded

--- src/Pages/Marketing/MarketingPages/GenCon22.js
+++ src/Pages/Marketing/MarketingPages/GenCon22.js
@@ -9,6 +9,14 @@ export default function GenCon22(){
             <p>Gen Con 2022 was a unique one because we did not have a major release at the convention to build our marketing around. The only new product we had to sell was the <a href='https://czechgames.com/en/galaxy-trucker-keep-on-trucking/' rel='noreferrer'> Galaxy Trucker: Keep on Trucking expansion</ a>. However, due to one of the other co-sponsors dropping out, this year we were one of only two major sponsors of the show, leading to us having a bigger exhibit room and more locations available to us for advertising.</p>
             <p>As we didn't have a big release, I made the call to focus our convention on increasing our name recognition. Many people know games like Codenames, Lost Ruins of Arnak, and Galaxy Trucker; but not nearly as many knew that CGE were the people behind many of their favorite games.</p>
             <p>My first step for this was securing a new advertising location for us with 3 large banners right by the main entrance and behind the very popular "Cardhalla" atraction:</p>
+            <div className="GC2pic">
+                <div className='GCpic1o2' >
+                    <img src='https://tylergoblebucket.s3.us-west-1.amazonaws.com/Gen+Con+2022/GC22+Banners+Above+Cardhalla+1.png' alt='Banners at Gen Con 2022 entrance' />
+                </div>
+                <div className='GCpic1o2' >
+                    <img src='https://tylergoblebucket.s3.us-west-1.amazonaws.com/Gen+Con+2022/Cardhalla+with+banners+1.png' alt='Cardhalla with CGE banners. Credit to @role_dice on Twitter' />
+                </div>
+            </div>
         </body>
     )
 }
